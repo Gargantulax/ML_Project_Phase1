337917799081ba7e3ca40523c2cf3790580b287d
Gargantulax <ar.shok80@gmail.com>
2023-04-20 12:50:46 +0330

    E-step file created

--- Q1.py
+++ Q1.py
@@ -3,28 +3,28 @@ import matplotlib.pyplot as plt
 
 file = open('Image1.csv')
 csvreader = csv.reader(file)
-data1_x = []
-data1_y = []
-data2_x = []
-data2_y = []
-data3_x = []
-data3_y = []
+data1_y1 = []
+data1_y2 = []
+data2_y1 = []
+data2_y2 = []
+data3_y1 = []
+data3_y2 = []
 for i in range(200):
     nxtln = next(csvreader)
-    data1_x.append(int(nxtln[0]))
-    data1_y.append(int(nxtln[1]))
+    data1_y1.append(float(nxtln[1]))
+    data1_y2.append(float(nxtln[2]))
 for i in range(200):
     nxtln = next(csvreader)
-    data2_x.append(int(nxtln[0]))
-    data2_y.append(int(nxtln[1]))
+    data2_y1.append(float(nxtln[1]))
+    data2_y2.append(float(nxtln[2]))
 for i in range(200):
     nxtln = next(csvreader)
-    data3_x.append(int(nxtln[0]))
-    data3_y.append(int(nxtln[1]))
+    data3_y1.append(float(nxtln[1]))
+    data3_y2.append(float(nxtln[2]))
 file.close()
 
-plt.scatter(data1_x, data1_y, color='r', label='Data 1')
-plt.scatter(data2_x, data2_y, color='g', label='Data 2')
-plt.scatter(data3_x, data3_y, color='b', label='Data 3')
+plt.scatter(data1_y1, data1_y2, color='r', label='Data 1')
+plt.scatter(data2_y1, data2_y2, color='g', label='Data 2')
+plt.scatter(data3_y1, data3_y2, color='b', label='Data 3')
 plt.legend()
 plt.show()
